21605b538249aaaae3dcb1612c460b18059b228e
GustavoGMM <gustavomaia855@gmail.com>
2023-11-02 11:53:49 -0300

    update

--- site/script.js
+++ site/script.js
@@ -2,10 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src ="imagens/menu.svg";
+        document.querySelector('.icon').src = "/imagens/menu.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "imagens/fechamento.svg";
+        document.querySelector('.icon').src = "/imagens/fechamento.svg";
     }
 }
-
